4da5901847e2a5edbe8e1fdf28e02b8c31ac3c61
imbrickiy <imbrickiy@ya.ru>
2022-10-31 16:46:05 +0300

    Перенос getcomment из утилит.Работа с формой.

--- js/form.js
+++ js/form.js
@@ -4,41 +4,47 @@ const bodyElement = document.querySelector('body');
 const formElement = document.querySelector('.img-upload__form');
 const formOverlayElement = formElement.querySelector('.img-upload__overlay');
 const btnCancel = formElement.querySelector('.img-upload__cancel');
+const btnUploadFile = formElement.querySelector('#upload-file');
 
-const pristine = new Pristine(formElement, {
-  classTo: 'img-upload__text',
-  errorTextParent: 'img-upload__text',
-  errorTextClass: 'img-upload__text__error-text'
-});
 
-function onEscKeydown (evt) {
-  if (isEcapeKey(evt)) {
+const initModal = () => {
+  const pristine = new Pristine(formElement, {
+    classTo: 'img-upload__text',
+    errorTextParent: 'img-upload__text',
+    errorTextClass: 'img-upload__text__error-text'
+  });
+
+  const openModal = () => {
+    formOverlayElement.classList.remove('hidden');
+    bodyElement.classList.add('modal-open');
+    document.addEventListener('keydown', onEscKeydown);
+  };
+
+  const closeModal = () => {
+    formElement.reset();
+    pristine.reset();
+    formOverlayElement.classList.add('hidden');
+    bodyElement.classList.remove('modal-open');
+    document.removeEventListener('keydown', onEscKeydown);
+  };
+
+  const onFileInputChange = () => openModal();
+
+  const onFormSubmit = (evt) => {
     evt.preventDefault();
-    closeModal();}
-}
+    pristine.validate();
+  };
 
-const openModal = () => {
-  formOverlayElement.classList.remove('hidden');
-  bodyElement.classList.add('modal-open');
-  document.addEventListener('keydown', onEscKeydown);};
+  btnUploadFile.addEventListener('change', onFileInputChange);
+  btnCancel.addEventListener('click', closeModal);
+  formElement.addEventListener('submit', onFormSubmit);
 
-function closeModal () {
-  formElement.reset();
-  pristine.reset();
-  formOverlayElement.classList.add('hidden');
-  bodyElement.classList.remove('modal-open');
-  document.removeEventListener('keydown', onEscKeydown);}
+  function onEscKeydown(evt) {
+    if (isEcapeKey(evt)) {
+      evt.preventDefault();
+      closeModal();
+    }
+  }
+};
 
-formElement.addEventListener('click', () => {
-  openModal();
-});
-
-btnCancel.addEventListener('click', () => {
-  closeModal();
-});
-
-formElement.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  // const valid = pristine.validate();
-  // заглушка
-});
+export { initModal };
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { initThumbnail } from './thumbnail.js';
 import { getSimilarWizards } from './mock.js';
-import './form.js';
+import { initModal } from './form.js';
 
 const data = getSimilarWizards();
 initThumbnail(data);
+initModal();
