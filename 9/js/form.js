dca503f04f4d6732a808f6c45bdc649f931ba9d4
Keks <keks@htmlacademy.ru>
2022-11-01 15:45:40 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/form.js
+++ 9/js/form.js
@@ -6,45 +6,45 @@ const formOverlayElement = formElement.querySelector('.img-upload__overlay');
 const btnCancel = formElement.querySelector('.img-upload__cancel');
 const btnUploadFile = formElement.querySelector('#upload-file');
 
+const pristine = new Pristine(formElement, {
+  classTo: 'img-upload__text',
+  errorTextParent: 'img-upload__text',
+  errorTextClass: 'img-upload__text__error-text'
+});
+
+const openModal = () => {
+  formOverlayElement.classList.remove('hidden');
+  bodyElement.classList.add('modal-open');
+  document.addEventListener('keydown', onEscKeydown);
+};
+
+const closeModal = () => {
+  formElement.reset();
+  pristine.reset();
+  formOverlayElement.classList.add('hidden');
+  bodyElement.classList.remove('modal-open');
+  document.removeEventListener('keydown', onEscKeydown);
+};
+
+const onFileInputChange = () => openModal();
+const onCancel = () => closeModal();
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+};
 
 const initModal = () => {
-  const pristine = new Pristine(formElement, {
-    classTo: 'img-upload__text',
-    errorTextParent: 'img-upload__text',
-    errorTextClass: 'img-upload__text__error-text'
-  });
-
-  const openModal = () => {
-    formOverlayElement.classList.remove('hidden');
-    bodyElement.classList.add('modal-open');
-    document.addEventListener('keydown', onEscKeydown);
-  };
-
-  const closeModal = () => {
-    formElement.reset();
-    pristine.reset();
-    formOverlayElement.classList.add('hidden');
-    bodyElement.classList.remove('modal-open');
-    document.removeEventListener('keydown', onEscKeydown);
-  };
-
-  const onFileInputChange = () => openModal();
-
-  const onFormSubmit = (evt) => {
-    evt.preventDefault();
-    pristine.validate();
-  };
-
   btnUploadFile.addEventListener('change', onFileInputChange);
-  btnCancel.addEventListener('click', closeModal);
+  btnCancel.addEventListener('click', onCancel);
   formElement.addEventListener('submit', onFormSubmit);
-
-  function onEscKeydown(evt) {
-    if (isEcapeKey(evt)) {
-      evt.preventDefault();
-      closeModal();
-    }
-  }
 };
+
+function onEscKeydown(evt) {
+  if (isEcapeKey(evt)) {
+    evt.preventDefault();
+    onCancel();
+  }
+}
 
 export { initModal };
